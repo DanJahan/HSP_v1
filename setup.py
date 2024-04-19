bf2ef4d791a371025c74446602fadb52d17b4e9c
DanJahan <dan.jahanian@gmail.com>
2024-04-19 19:35:37 +0000

    First commit setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Setup for the HSP Calculator package."""
+from setuptools import setup
+
+setup(
+    name="HSP_v1",
+    version="0.0.1",
+    description="HSP_v1 setup",
+    maintainer="Dan Jahanian",
+    maintainer_email="dan.jahanian@gmail.com",
+    license="MIT",
+    packages=["HSP_v1"],
+    scripts=["HSP_Calc.py"],
+    long_description="""Hansen Solubility Parameter Calculator""",
+)
